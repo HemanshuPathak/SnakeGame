1a38aff4720bef5a12df6db20e97d243d00ed159
Hemanshu <himanshu2002pathak@gmail.com>
2024-01-09 01:03:18 +0530

    Main Files
    Snake Game Project Files

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+using Quaternion = UnityEngine.Quaternion;
+using Random = UnityEngine.Random;
+using Vector3 = UnityEngine.Vector3;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance = null;
+
+    const float width = 3.7f;
+    const float height = 7f;
+
+    public float snakeSpeed = 1;
+
+    public BodyPart bodyPrefab = null;
+    public GameObject rockPrefab = null;
+    public GameObject eggPrefab = null;
+    public GameObject goldEggPrefab = null;
+    public GameObject spikePrefab = null;
+
+    public Sprite tailSprite = null;
+
+    public Sprite bodySprite = null;
+
+    public SnakeHead snakeHead = null;
+
+    public bool alive = true;
+
+    public bool waitingToPlay = true;
+
+    List<Egg> eggs = new List<Egg>();
+    List<Spike> spikes = new List<Spike>();
+
+    public int level = 0;
+    int spikecount = 0;
+    int noOfEggsForNextLevel = 0;
+
+    public int score = 0;
+    public int hiScore = 0;
+
+    public Text scoreText = null;
+    public Text hiScoreText = null;
+
+    public Text tapToPlayText = null;
+    public Text gameOverText = null;
+    public Text assignmentByText = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        CreateWalls();
+        alive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitingToPlay)
+        {
+            foreach(Touch touch in Input.touches)
+            {
+                if(touch.phase == TouchPhase.Ended)
+                {
+                    StartGamePlay();
+                }
+            }
+
+            if (Input.GetMouseButtonUp(0))
+                StartGamePlay();
+        }
+    }
+
+   
+
+    public void GameOver()
+    {
+        alive = false;
+        waitingToPlay = true;
+        gameOverText.gameObject.SetActive(true);
+        tapToPlayText.gameObject.SetActive(true);
+        assignmentByText.gameObject.SetActive(true);
+
+    }
+
+    void StartGamePlay()
+    {
+        score = 0;
+        level = 0;
+        scoreText.text = "Score = " + score;
+        hiScoreText.text = "HiScore = " + hiScore;
+
+        gameOverText.gameObject.SetActive(false);
+        tapToPlayText.gameObject.SetActive(false);
+        assignmentByText.gameObject.SetActive(false);
+
+        waitingToPlay = false;
+        alive = true;
+        KillOldEggs();
+        KillOldSpikes();
+        LevelUp();
+    }
+
+    void LevelUp()
+    {
+        level++;
+
+        noOfEggsForNextLevel = 4 + (level * 2);
+
+        snakeSpeed = 1f + (level/4f);
+        if (snakeSpeed > 6) snakeSpeed = 6;
+
+        snakeHead.ResetSnake();
+        CreateEgg();
+        CreateSpike();
+    }
+
+    public void EggEaten(Egg egg)
+    {
+        score++;
+
+        noOfEggsForNextLevel--;
+        if (noOfEggsForNextLevel == 0)
+        {
+            score += 10;
+            LevelUp();
+            spikecount++;
+        }
+        else if (noOfEggsForNextLevel == 1)
+            CreateEgg(true);
+        else
+            CreateEgg(false);
+
+        if (score > hiScore)
+        {
+            hiScore = score;
+            hiScoreText.text = "HiScore = " + hiScore;
+        }
+
+        scoreText.text = "Score = " + score;
+
+        eggs.Remove(egg);
+        Destroy(egg.gameObject);
+    }
+
+    void CreateWalls()
+    {
+        float z = -1f;
+
+        Vector3 start = new Vector3(-width, -height, z);
+        Vector3 finish = new Vector3(-width, +height, z);
+        CreateWall(start, finish);
+
+        start = new Vector3(+width, -height, z);
+        finish = new Vector3(+width, +height, z);
+        CreateWall(start, finish);
+
+        start = new Vector3(-width, -height, z);
+        finish = new Vector3(+width, -height, z);
+        CreateWall(start, finish);
+
+        start = new Vector3(-width, +height, z);
+        finish = new Vector3(+width, +height, z);
+        CreateWall(start, finish);
+    }
+
+    void CreateWall(Vector3 start, Vector3 finish)
+    {
+        float distance = Vector3.Distance(start, finish);
+        int noOfRocks = (int)(distance * 3f);
+        Vector3 delta = (finish-start)/noOfRocks;
+        Vector3 position = start;
+        for(int rock = 0; rock <= noOfRocks; rock++)
+        {
+            float rotation = Random.Range(0, 360f);
+            float scale = Random.Range(1.5f, 2f);
+            CreateRock(position, scale, rotation);
+            position = position + delta;
+        }
+    }
+
+    void CreateRock(Vector3 position, float scale, float rotation)
+    {
+        GameObject rock = Instantiate(rockPrefab, position, Quaternion.Euler(0, 0, rotation));
+        rock.transform.localScale = new Vector3(scale, scale, 1);
+    }
+
+
+    void CreateEgg(bool golden = false)
+    {
+        Vector3 position;
+        position.x = -width + Random.Range(1f, (width * 2) - 2f);
+        position.y = -height + Random.Range(1f, (height * 2) - 2f);
+        position.z = -1f;
+        Egg egg = null;
+        if(golden)
+            egg = Instantiate(goldEggPrefab, position, quaternion.identity).GetComponent<Egg>();
+        else
+            egg = Instantiate(eggPrefab, position, quaternion.identity).GetComponent<Egg>();
+
+        eggs.Add(egg);
+
+    }
+
+    void CreateSpike()
+    {
+        Vector3 position;
+        position.x = -width + Random.Range(1f, (width * 2) - 2f);
+        position.y = -height + Random.Range(1f, (height * 2) - 2f);
+        position.z = -1f;
+        Spike spike = null;
+        
+            spike = Instantiate(spikePrefab, position, quaternion.identity).GetComponent<Spike>();
+        spikes.Add(spike);
+    }
+
+    void KillOldEggs()
+    {
+        foreach(Egg egg in eggs)
+        {
+            Destroy(egg.gameObject);
+        }
+        eggs.Clear();
+    }
+
+    void KillOldSpikes()
+    {
+        foreach (Spike spike in spikes)
+        {
+            Destroy(spike.gameObject);
+        }
+        spikes.Clear();
+    }
+
+}
